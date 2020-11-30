1146aff563b1594effd634bc3375a3bedbbe8d53
Sam Hofius <sam@samhofi.us>
2020-11-30 09:40:10 -0500

    Add documentation to util package

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -8,6 +8,7 @@ import (
 	"samhofi.us/x/keybase/v2/types/chat1"
 )
 
+// StringInSlice returns true if the given string is present in the slice of strings
 func StringInSlice(needle string, haystack []string) bool {
 	for _, item := range haystack {
 		if item == needle {
@@ -17,6 +18,7 @@ func StringInSlice(needle string, haystack []string) bool {
 	return false
 }
 
+// ChannelString returns a string representation of a chat1.ChatChannel suitable for inclusion in chat and log messages
 func ChannelString(channel chat1.ChatChannel) string {
 	if channel.MembersType == keybase.USER {
 		return channel.Name
@@ -25,6 +27,7 @@ func ChannelString(channel chat1.ChatChannel) string {
 	return fmt.Sprintf("%s#%s", channel.Name, channel.TopicName)
 }
 
+// HasMinRole returns true if the given user has the given role or higher in the converation
 func HasMinRole(kb *keybase.Keybase, role string, user string, conv chat1.ConvIDStr) bool {
 	conversation, err := kb.ListMembersOfConversation(conv)
 	if err != nil {

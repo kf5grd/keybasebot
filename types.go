f1d93d62924b9daca4d4b8797b0c96cbf3daa3fe
Sam Hofius <sam@samhofi.us>
2020-12-01 12:29:23 -0500

    Clean up docstrings in types.go

--- types.go
+++ types.go
@@ -17,9 +17,15 @@ type BotAction func(chat1.MsgSummary, *Bot) (bool, error)
 
 // BotCommand holds information regarding a command and its advertisements
 type BotCommand struct {
-	Name string                     // Name of the command for use in the logs
-	Ad   *chat1.UserBotCommandInput // This will show in your chat channels when someone starts typing a command in the message box
-	Run  BotAction                  // The function to run when the command is triggered
+	// Name of the command for use in the logs
+	Name string
+
+	// This will show in your chat channels when someone starts typing a command in the
+	// message box. Set this to nil if you don't want the command advertised
+	Ad *chat1.UserBotCommandInput
+
+	// The function to run when the command is triggered
+	Run BotAction
 }
 
 // Adapter can modify the behavior of a BotAction
@@ -27,21 +33,46 @@ type Adapter func(BotAction) BotAction
 
 // Bot is where we'll hold the necessary information for the bot to run
 type Bot struct {
-	Name          string                 // This will show up next to the bot's username in chat messages
-	CommandPrefix string                 // If this is set, any messages that are received with a Text type that do not have this string prefix will be discarded. This can be useful if all of your commands start with the same prefix.
-	KB            *keybase.Keybase       // The Keybase instance
-	Logger        *logr.Logger           // The logr instance
-	LogWriter     io.Writer              // Where you want log messages sent
-	JSON          bool                   // Whether log messages should be in JSON format
-	Debug         bool                   // Whether to show debug messages in log output
-	Handlers      keybase.Handlers       // Message handlers. You probably should leave the Chat handler alone
-	Opts          keybase.RunOptions     // Custom run options for the message listener
-	Commands      []BotCommand           // A slice holding all of you BotCommands. Be sure to populate this prior to calling Run()
-	Meta          map[string]interface{} // You can use this to store custom info in order to pass it around to your bot commands
+	// The Name string will show up next to the bot's username in chat messages. Setting this
+	// to empty will cause it to be ignored
+	Name string
+
+	// If CommandPrefix is set, any messages that are received with a TypeName of "text," and
+	// do not have this string prefix will be discarded. This can be useful if all of your
+	// commands start with the same prefix.
+	CommandPrefix string
+
+	// The Keybase instance
+	KB *keybase.Keybase
+
+	// The logr instance
+	Logger *logr.Logger
+
+	// Where you want log messages written to
+	LogWriter io.Writer
+
+	// Whether log messages should be in JSON format
+	JSON bool
+
+	// Whether to show debug messages in log output
+	Debug bool
+
+	// Message handlers. You probably should leave the Chat handler alone
+	Handlers keybase.Handlers
+
+	// Custom run options for the message listener
+	Opts keybase.RunOptions
+
+	// A slice holding all of you BotCommands. Be sure to populate this prior to calling Run()
+	Commands []BotCommand
+
+	// You can use this to store custom info in order to pass it around to your bot commands
+	Meta map[string]interface{}
 }
 
-// New returns a new Bot instance. name will set the Bot.Name and will show up next to the bot's username in chat messages. You can set name to an empty string.
-// You can also pass in any keybase.KeybaseOpt options and they will be passed to keybase.New()
+// New returns a new Bot instance. name will set the Bot.Name and will show up next to the
+// bot's username in chat messages. You can set name to an empty string. You can also pass
+// in any keybase.KeybaseOpt options and they will be passed to keybase.New()
 func New(name string, opts ...keybase.KeybaseOpt) *Bot {
 	var b Bot
 	b.Name = name

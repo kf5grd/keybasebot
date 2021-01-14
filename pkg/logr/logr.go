15d2f3183ef5d6759fe23b5eb9fc6fb4a89f2193
Sam Hofius <sam@samhofi.us>
2021-01-13 19:27:53 -0500

    Writing logs should return a logr.Msg instead of a string

--- pkg/logr/logr.go
+++ pkg/logr/logr.go
@@ -37,46 +37,49 @@ func getCaller() string {
 	return frame[len(frame)-1]
 }
 
+func (m Msg) String() string {
+	return fmt.Sprint(toJson(m) + "\n")
+}
+
 // Write writes the log string to the Logger.Writer with the given name string, and Level, and returns a string with the same output in case you want to do something else with it
-func (l *Logger) Write(name string, level Level, s string, a ...interface{}) string {
-	var ret string
+func (l *Logger) Write(name string, level Level, s string, a ...interface{}) Msg {
+	var msg Msg
 	var (
 		now        = time.Now().UTC()
 		timeFormat = "02Jan2006 15:04:05"
 	)
 
+	msg = Msg{
+		Time:     now.Unix(),
+		FuncName: name,
+		Level:    level.String(),
+		Message:  fmt.Sprintf(s, a...),
+	}
+
 	if l.JSON {
-		msg := Msg{
-			Time:     now.Unix(),
-			FuncName: name,
-			Level:    level.String(),
-			Message:  fmt.Sprintf(s, a...),
-		}
-		ret = toJson(msg) + "\n"
-		fmt.Fprint(l.Writer, ret)
-		return ret
+		// As of now, this is the same thing as calling `msg.String()`, but it's very possible
+		// the format of the stringer could change in the future, and this needs to always return
+		// json, so I've explicitly chosen not to call `msg.String()` here
+		fmt.Fprint(l.Writer, toJson(msg)+"\n")
+		return msg
 	}
 
 	a = append([]interface{}{strings.ToUpper(now.Format(timeFormat)), name, level}, a...)
-	ret = fmt.Sprintf("[%v][%s] %v: "+s+"\n", a...)
-	fmt.Fprint(l.Writer, ret)
-	return ret
+	fmt.Fprint(l.Writer, fmt.Sprintf("[%v][%s] %v: "+s+"\n", a...))
+	return msg
 }
 
 // Error sets the Level to LevelError, and automatically sets the name of the caller, then calls Write
-func (l *Logger) Error(s string, a ...interface{}) string {
+func (l *Logger) Error(s string, a ...interface{}) Msg {
 	return l.Write(getCaller(), LevelError, s, a...)
 }
 
 // Info sets the Level to LevelInfo, and automatically sets the name of the caller, then calls Write
-func (l *Logger) Info(s string, a ...interface{}) string {
+func (l *Logger) Info(s string, a ...interface{}) Msg {
 	return l.Write(getCaller(), LevelInfo, s, a...)
 }
 
 // Debug sets the Level to LevelDebug, and automatically sets the name of the caller, then calls Write only if Logger.EnableDebug is true
-func (l *Logger) Debug(s string, a ...interface{}) string {
-	if !l.EnableDebug {
-		return ""
-	}
+func (l *Logger) Debug(s string, a ...interface{}) Msg {
 	return l.Write(getCaller(), LevelDebug, s, a...)
 }
